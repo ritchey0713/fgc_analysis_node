f7589d732adcb17ad25b3ea7a1674ea0e79c3ca9
Dj <ritchey0713@gmail.com>
2021-02-24 10:02:51 -0500

    add seed file config

---
+++ seed.js
@@ -0,0 +1,5 @@
+module.exports = {
+  undefined: "mongodb://localhost/fgc_analysis_backend",
+  dev: "mongodb://localhost/fgc_analysis_backend",
+  prod: "mongodb://localhost/fgc_analysis_backend",
+};
