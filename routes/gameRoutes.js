20551a94860f38c4ba834270370eab0606c8a6fe
Dj <ritchey0713@gmail.com>
2021-02-24 15:38:45 -0500

    add new routes

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,5 +1,6 @@
 // const requireLogin = require("../middlewares/requireLogin");
 const mongoose = require("mongoose");
+const { reset } = require("nodemon");
 // const requireCredits = require("../middlewares/requireCredits");
 const Game = mongoose.model("games");
 const Character = mongoose.model("characters");
@@ -12,7 +13,21 @@ module.exports = (app) => {
         .map((nameEle) => nameEle[0].toUpperCase() + nameEle.substring(1))
         .join(" ");
       const game = await Game.find({ title: normalizedName });
-      res.send(game);
+      res.send(game[0]);
+    } catch (err) {
+      console.log("ERR::::", err);
+    }
+  });
+
+  app.get("/api/games/:game_name/characters", async (req, res) => {
+    try {
+      const normalizedName = req.params.game_name
+        .split("_")
+        .map((nameEle) => nameEle[0].toUpperCase() + nameEle.substring(1))
+        .join(" ");
+      const game = await Game.find({ title: normalizedName });
+      const characters = await Character.find({ gameId: game[0]._id });
+      res.send({ char: characters[0] });
     } catch (err) {
       console.log("ERR::::", err);
     }
