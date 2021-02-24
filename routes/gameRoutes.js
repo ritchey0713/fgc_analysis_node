3b92788629dee204cd88e6b1c2d531ffaa906d75
Dj <ritchey0713@gmail.com>
2021-02-24 15:54:36 -0500

    refactor slug normalization

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -4,15 +4,18 @@ const { reset } = require("nodemon");
 // const requireCredits = require("../middlewares/requireCredits");
 const Game = mongoose.model("games");
 const Character = mongoose.model("characters");
+const { normalizeSlug } = require("../services/normalize.js");
 
 module.exports = (app) => {
   app.get("/api/games/:game_name", async (req, res) => {
     try {
-      const normalizedName = req.params.game_name
-        .split("_")
-        .map((nameEle) => nameEle[0].toUpperCase() + nameEle.substring(1))
-        .join(" ");
-      const game = await Game.find({ title: normalizedName });
+      // const normalizedName = req.params.game_name
+      //   .split("_")
+      //   .map((nameEle) => nameEle[0].toUpperCase() + nameEle.substring(1))
+      //   .join(" ");
+      const game = await Game.find({
+        title: normalizeSlug(req.params.game_name),
+      });
       res.send(game[0]);
     } catch (err) {
       console.log("ERR::::", err);
@@ -21,13 +24,15 @@ module.exports = (app) => {
 
   app.get("/api/games/:game_name/characters", async (req, res) => {
     try {
-      const normalizedName = req.params.game_name
-        .split("_")
-        .map((nameEle) => nameEle[0].toUpperCase() + nameEle.substring(1))
-        .join(" ");
-      const game = await Game.find({ title: normalizedName });
+      // const normalizedName = req.params.game_name
+      //   .split("_")
+      //   .map((nameEle) => nameEle[0].toUpperCase() + nameEle.substring(1))
+      //   .join(" ");
+      const game = await Game.find({
+        title: normalizeSlug(req.params.game_name),
+      });
       const characters = await Character.find({ gameId: game[0]._id });
-      res.send({ char: characters[0] });
+      res.send({ char: characters });
     } catch (err) {
       console.log("ERR::::", err);
     }
