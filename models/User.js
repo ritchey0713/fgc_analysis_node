359403e267d7fe626598caaa1344436bf22c8b3b
Dj <ritchey0713@gmail.com>
2021-02-24 10:03:12 -0500

    set basic schema

--- models/User.js
+++ models/User.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const gameSchema = require("./Game");
 
 const userSchema = new Schema({
   googleId: String,
+  games: [gameSchema],
+  age: Number,
+  ranking: Number,
+  years_played: Number,
+  competing: Boolean,
 });
 
 mongoose.model("users", userSchema);
