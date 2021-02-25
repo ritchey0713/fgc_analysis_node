c241999b5061934c404379c3e6a34fe1dc61576b
Dj <ritchey0713@gmail.com>
2021-02-25 15:12:20 -0500

    add collations

--- models/Character.js
+++ models/Character.js
@@ -1,16 +1,31 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const characterSchema = new Schema({
-  name: String,
-  type: String,
-  moves: Schema.Types.Mixed,
-  stats: Schema.Types.Mixed,
-  beginnerNotes: String,
-  tier: String,
-  banned: Boolean,
-  isDLC: Boolean,
-  gameId: String,
-});
+const characterSchema = new Schema(
+  {
+    name: { type: String, index: true },
+    type: String,
+    moves: Schema.Types.Mixed,
+    stats: Schema.Types.Mixed,
+    beginnerNotes: String,
+    tier: String,
+    banned: Boolean,
+    isDLC: Boolean,
+    gameId: String,
+  },
+  { collation: { locale: "en_US", strength: 2 } }
+);
+
+// characterSchema.createIndex({ name: "text" }, (err, res) => {
+//   console.log(res);
+// });
+
+characterSchema.index(
+  { name: 1 },
+  { collation: { locale: "en_US", strength: 2 } },
+  (err, res) => {
+    console.log(res);
+  }
+);
 
 mongoose.model("characters", characterSchema);
